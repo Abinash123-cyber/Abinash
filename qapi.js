85d4bf41a9c475eb3238c5951f2b4161324d6954
Abinash123-cyber <75572357+Abinash123-cyber@users.noreply.github.com>
2020-12-06 19:07:41 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fccdb0b057d16001e794148",
+				"5fccdb0b057d16001e79414a"
+			]
+		},
+		"5fccdb0b057d16001e794148": {
+			"id": "5fccdb0b057d16001e794148",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fccdb0b057d16001e79414a": {
+			"id": "5fccdb0b057d16001e79414a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
